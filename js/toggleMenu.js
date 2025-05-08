34880a2f55724b2bb9b42e909648b972570df2bf
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2025-05-08 16:57:40 -0300

    fix: toggle menu

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -4,3 +4,9 @@ const sidebar = document.getElementById("sidebar");
 toggleMenu.addEventListener("click", () => {
   sidebar.classList.toggle("open");
 });
+
+document.addEventListener("click", (event) => {
+  if (!sidebar.contains(event.target) && !toggleMenu.contains(event.target)) {
+    sidebar.classList.remove("open");
+  }
+});
