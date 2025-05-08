8cc40139ebb9b8bbfde33c8cbb198322366b5754
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2025-05-08 18:00:24 -0300

    fix: assets path

--- js/togglePassword.js
+++ js/togglePassword.js
@@ -5,7 +5,7 @@ const eyeIcon = document.getElementById("eyeIcon");
 togglePassword.addEventListener("click", () => {
   const isVisible = passwordInput.type === "text";
   passwordInput.type = isVisible ? "password" : "text";
-  eyeIcon.src = isVisible ? "/assets/eye-off.svg" : "/assets/eye.svg";
+  eyeIcon.src = isVisible ? "/assets/ui/eye-off.svg" : "/assets/eye.svg";
   togglePassword.setAttribute(
     "aria-label",
     isVisible ? "Mostra senha" : "Oculta senha"
